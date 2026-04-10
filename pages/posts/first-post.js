b3aceed066085241a888b870df21ce98f50e10a6
ayman <ayman.sale000@gmail.com>
2026-04-10 21:42:26 +0200

    update profile and introduction

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
@@ -8,9 +9,14 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLazy={() => console.log('Script has been loaded, window.FB has been populated')}
+      />
       <h1>First Post</h1>
           <h2> 
-            <Link href="/">Home</Link>
+            <Link href="/">Back to Home</Link>
           </h2>
     </Layout>
   );
