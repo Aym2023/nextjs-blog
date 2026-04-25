88450f1eade47ded0dd1f5127ed5f8f329ef6e88
ayman <ayman.sale000@gmail.com>
2026-04-25 15:21:11 +0300

    udate date component

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,8 +1,19 @@
 import { parseISO, format } from "date-fns";
 
-export default function Date({ dateString }) {
-    const parsedDate = parseISO(dateString);
-    return <time dateTime={dateString}>{format(parsedDate, "LLLL d, yyyy")}</time>;
+import { parseISO, format } from 'date-fns'
+
+export default function DateComponent({ dateString }) {
+  if (!dateString) {
+    return <span>No date available</span>
+  }
+  
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
+
+// export default function Date({ dateString }) {
+//     const parsedDate = parseISO(dateString);
+//     return <time dateTime={dateString}>{format(parsedDate, "LLLL d, yyyy")}</time>;
+// }
 
 
